964ae02db5bc8d685154d730190ee486ac06acff
dyiar <austin.hl.blake@gmail.com>
2019-02-01 11:09:51 -0700

    ran into a weird issue when trying to test the response to my get request. it would return all the header content. found a test that worked sorta but still dont know why that happened

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.get("/", (req, res) => {
   res.status(200).json({ sanity: "check" });
 });
 
-server.post("/", (req, res) => {
+server.post("/games", (req, res) => {
   if (req.body.title == null || req.body.genre == null) {
     res.status(422).send({ error: "information incomplete " });
   } else {
@@ -27,5 +27,12 @@ server.post("/", (req, res) => {
       .catch(() => res.status(500).send({ error: "data not saved" }));
   }
 });
+
+server.get('/games', (req, res) => {
+    db('games').then(game => {
+        res.status(200).send(game)
+    })
+    .catch(() => res.status(500).json({ error: 'cant get games'}))
+})
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,33 +1,68 @@
-const request = require('supertest');
-const server = require('./server');
+const request = require("supertest");
+const server = require("./server");
 
-describe('server.js tests', () => {
-    describe('GET / endpoint', () => {
-        it('should respond with status code 200 OK', async () => {
-            let response = await request(server).get('/');
-            expect(response.status).toBe(200);
-            expect(response.type).toMatch(/json/i)
-        })
+afterEach(async () => {
+  await db("games").truncate();
+});
 
-    })
+describe("server.js tests", () => {
+  describe("GET / endpoint", () => {
+    it("should respond with status code 200 OK", async () => {
+      let response = await request(server).get("/");
+      expect(response.status).toBe(200);
+      expect(response.type).toMatch(/json/i);
+    });
+  });
 
-    describe('POST / endpoint', () => {
-        it('should respond with status code 201 OK', async () => {
-            let response = await request(server).post('/').send({ title: 'League of Legends', genre: 'MOBA', releaseYear: 2010 })
+  describe("POST /GAMES endpoint", () => {
+    it("should respond with status code 201 OK", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({ title: "League of Legends", genre: "MOBA", releaseYear: 2010 });
 
-            expect(response.status).toBe(201);
-        })
+      expect(response.status).toBe(201);
+    });
 
-        it('should respond with status code 422 if information is incomplete', async () => {
-            let response = await request(server).post('/').send({ title: 'DOTA' })
+    it("should respond with status code 422 if information is incomplete", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({ title: "DOTA" });
 
-            expect(response.status).toBe(422)
-        })
+      expect(response.status).toBe(422);
+    });
 
-        it('should send back response as json', async () => {
-            let response = await request(server).post('/').send({ title: 'DOTA', genre: 'MOBA' })
+    it("should send back response as json", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({ title: "DOTA", genre: "MOBA" });
 
-            expect(response.type).toMatch(/json/i)
-        })
-    })
-})
+      expect(response.type).toMatch(/json/i);
+    });
+  });
+
+  describe("GET /GAMES endpoint", () => {
+    it("should respond with status code 200 OK", async () => {
+      let response = await request(server).get("/games");
+
+      expect(response.status).toBe(200);
+    });
+
+    it("should get a list of games", async () => {
+      let league = await request(server)
+        .post("/games")
+        .send({ title: "League of Legends", genre: "MOBA", releaseYear: 2010 });
+      let dota = await request(server)
+        .post("/games")
+        .send({ title: "DOTA", genre: "MOBA" });
+        let response = await request(server).get("/games");
+
+      expect(response.text).toContain("League of Legends")
+      expect(response.text).toContain("DOTA")
+    });
+
+    it("should return an empty array if nothing is there", async () => {
+      let response = await request(server).get("/games");
+      expect(response.text).toEqual('[]');
+    });
+  });
+});
