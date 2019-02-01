e31853500006a4530e9e48231641e9734bc2abea
dyiar <austin.hl.blake@gmail.com>
2019-02-01 10:19:02 -0700

    added tests for insert and get functions. 1 test is shared between the two, and each two have 2 unique tests.

--- games/games.spec.js
+++ games/games.spec.js
@@ -6,3 +6,21 @@ const gameFile = require('./games')
 afterEach(async () => {
     await db('games').truncate();
 })
+
+describe('games testing', () => {
+    it('should insert a game', async () => {
+        const game = await gameFile.insert({ title: 'Final Fantasy VII', genre: 'RPG' })
+        const game2 = await gameFile.insert({ title: 'World of Warcraft', genre: 'MMORPG' })
+
+        const games = await db('games');
+        expect(games).toHaveLength(2)
+        expect(game.title).toEqual('Final Fantasy VII')
+        expect(game2.genre).toEqual('MMORPG')
+    })
+
+    it('should return an empty array if nothing is there', async () => {
+        const response = await db('games');
+        expect(response).toHaveLength(0);
+        expect(response).toEqual([])
+    })
+})
